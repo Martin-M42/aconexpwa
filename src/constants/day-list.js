9d0d70f2c0c6a7db1e24bd30c1ab35dd765abafd
LionelCMZ <moraleslionel792@gmail.com>
2024-09-19 22:59:15 -0400

    Corrección no aparece el día domingo en los horarios

--- src/constants/day-list.js
+++ src/constants/day-list.js
@@ -1,12 +1,12 @@
 // NO utilizar. Debe ser removido del codigo en un futuro
 export const dayList = {
+	0: 'Domingo',
 	1: 'Lunes',
 	2: 'Martes',
 	3: 'Miércoles',
 	4: 'Jueves',
 	5: 'Viernes',
 	6: 'Sábado',
-	7: 'Domingo',
 };
 
 // Objeto que debe utilizarse, porque con este formato trabaja DayJs internamente
